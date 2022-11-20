b730f7e575599b31c1a60ebaa830954c24f40161
GondiMartin <martin.gondocs@gmail.com>
2022-11-20 13:23:32 +0100

    Now it works! - TestActivity

--- QuizMaker/app/src/main/java/hu/bme/aut/android/quizmaker/test/activity/TestActivity.kt
+++ QuizMaker/app/src/main/java/hu/bme/aut/android/quizmaker/test/activity/TestActivity.kt
@@ -3,14 +3,19 @@ package hu.bme.aut.android.quizmaker.test.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import hu.bme.aut.android.quizmaker.databinding.ActivityTestBinding
+import hu.bme.aut.android.quizmaker.question.adapter.QuestionAdapter
+import hu.bme.aut.android.quizmaker.question.data.QuestionDatabase
 import hu.bme.aut.android.quizmaker.test.adapter.TestAdapter
 import hu.bme.aut.android.quizmaker.test.data.TestDatabase
+import kotlin.concurrent.thread
 
 class TestActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityTestBinding
-    private lateinit var adapter: TestAdapter
+    private lateinit var test_adapt: TestAdapter
+    private lateinit var quest_adapt: QuestionAdapter
     private lateinit var test_db: TestDatabase
+    private lateinit var quest_db: QuestionDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +24,21 @@ class TestActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         test_db = TestDatabase.getDatabase(applicationContext)
+        quest_db = QuestionDatabase.getDatabase(applicationContext)
+        test_adapt = TestAdapter()
+        quest_adapt = QuestionAdapter()
+
+        loadItemsInBackground()
+    }
+
+    private fun loadItemsInBackground() {
+        thread {
+            val testItems = test_db.testItemDao().getAll()
+            val questItems = quest_db.questionItemDao().getAll()
+            runOnUiThread {
+                test_adapt.update(testItems)
+                quest_adapt.update(questItems)
+            }
+        }
     }
 }
